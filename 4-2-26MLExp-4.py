e6e74b2797f46135324bc1815c336ba3d5ea21e8
dedbloop101 <142652250+dedbloop101@users.noreply.github.com>
2026-02-04 11:13:34 +0530

    Add files via upload

---
+++ 4-2-26MLExp-4.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+# Feature (X) and Target (y)
+
+X = np.array([[1], [2], [3], [4], [5]])
+y = np.array([2, 4, 5, 4, 5])
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+
+print("Mean Squared Error (MSE):", mse)
+
+plt.scatter(X, y, color='blue', label="Actual Data")
+plt.plot(X, model.predict(X), color='red', label='Regression Line')
+
+plt.xlabel("X")
+plt.ylabel("y")
+plt.legend()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
